2b61705cdde76087fa9df4d739ba62457f1d2df1
Jason Stephen <jason@Jasons-MacBook-Pro.local>
2019-03-10 22:11:23 -0500

    first commit

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Jason Stephen on 3/10/19.
+//  Copyright © 2019 Jason Stephen. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
