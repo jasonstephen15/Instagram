ccfecb92da2ac6f5f0c74bbf902c5efc799dc967
Jason Stephen <jason@Jasons-MacBook-Pro.local>
2019-03-23 19:48:47 -0500

    comments

---
+++ Instagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Instagram
+//
+//  Created by Jason Stephen on 3/21/19.
+//  Copyright © 2019 Jason Stephen. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
